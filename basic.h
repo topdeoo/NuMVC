dd4d864aa0fbe82c67a1b71680cb2a44714227cc
Virgil Ling <virgil7@gmail.com>
2023-05-24 23:59:18 +0800

    add: code comments

--- basic.h
+++ basic.h
@@ -1,89 +1,86 @@
-//
-// Created by LENOVO on 2023/3/11.
-//
+//! @file basic data structure
+//! @author Virgil
 
 #ifndef PDSP_BASIC_H
 #define PDSP_BASIC_H
 
+// Data Structure
 
-//! Data Structure
-
-#include <unordered_map>
-#include <unordered_set>
+#include <algorithm>
 #include <chrono>
-#include <vector>
-#include <set>
-#include <stack>
 #include <cmath>
 #include <fstream>
 #include <iostream>
-#include <random>
 #include <map>
 #include <queue>
-#include <algorithm>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
-int n, m; //! n: number of nodes, m: number of edges
+//! @var n: number of nodes
+//! @var m: number of edges
+int n, m;
 
-//! safe and fast hash function
-//! reference: https://codeforces.com/blog/entry/62393
+//! @struct custom_hash, a safe and fast hash function
+//! @link https://codeforces.com/blog/entry/62393
 
 struct custom_hash {
-    static uint64_t splitmix64 (uint64_t x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
+  static uint64_t splitmix64(uint64_t x) {
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
 
-    size_t operator() (uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
+  size_t operator()(uint64_t x) const {
+    static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
 };
 
-
-//! bitmap for support set
+//! @brief bitmap for support set
 struct bitmap {
-    std::vector<size_t> _bit;
+  std::vector<size_t> _bit;
 
-    bitmap () = default;
+  bitmap() = default;
 
-    explicit bitmap (size_t size) : _bit(size / 32 + 1) {}
+  explicit bitmap(size_t size) : _bit(size / 32 + 1) {}
 
-    void set (const size_t &pos) {
-        size_t index = pos / 32;
-        size_t offset = pos % 32;
-        _bit[index] |= (1 << offset);
+  void set(const size_t &pos) {
+    size_t index = pos / 32;
+    size_t offset = pos % 32;
+    _bit[index] |= (1 << offset);
+  }
+
+  bool find(const size_t &pos) {
+    size_t index = pos / 32;
+    size_t offset = pos % 32;
+    return (_bit[index] >> offset) & 1;
+  }
+
+  void reset(const size_t &pos) {
+    size_t index = pos / 32;
+    size_t offset = pos % 32;
+    _bit[index] &= ~(1 << offset);
+  }
+
+  // judge the vertex u whether is only domain by vertex v
+  bool is_only() {
+    int cnt = 0;
+    for (auto &i : _bit) {
+      if (i != 0) cnt++;
     }
-
-    bool find (const size_t &pos) {
-        size_t index = pos / 32;
-        size_t offset = pos % 32;
-        return (_bit[index] >> offset) & 1;
-    }
-
-    void reset (const size_t &pos) {
-        size_t index = pos / 32;
-        size_t offset = pos % 32;
-        _bit[index] &= ~(1 << offset);
-    }
-
-    //! judge the vertex u whether is only domain by vertex v
-    bool is_only () {
-        int cnt = 0;
-        for (auto &i: _bit) {
-            if (i != 0)
-                cnt++;
-        }
-        return cnt == 1;
-    }
-
+    return cnt == 1;
+  }
 };
 
-std::unordered_set<int, custom_hash> C; //! candidate set
-std::unordered_set<int, custom_hash> P; //! observed set
-std::unordered_map<int, std::unordered_set<int, custom_hash>, custom_hash> S; //! support set
-std::unordered_map<int, int> Link; //! record the vertex is supported by which vertex
+std::unordered_set<int, custom_hash> C;                                        //! candidate set
+std::unordered_set<int, custom_hash> P;                                        //! observed set
+std::unordered_map<int, std::unordered_set<int, custom_hash>, custom_hash> S;  //! support set
+std::unordered_map<int, int> Link;  //! record the vertex is supported by which vertex
 
 //! edges
 std::unordered_map<int, std::vector<int>, custom_hash> edges;
@@ -91,45 +88,46 @@ std::unordered_map<int, std::vector<int>, custom_hash> edges;
 //! neighbor list
 std::unordered_map<int, std::vector<int>, custom_hash> neighbor;
 
-
+//! @struct vertex
+//! @details
 //! score for vertex, sorted by score in increasing order
 //! add_score is end()
 //! remove_score is begin()
 struct vertex {
-    int score, age;
-    vertex () = default;
-    explicit vertex(int score, int age) : score(score), age(age) {}
+  int score, age;
+  vertex() = default;
+  explicit vertex(int score, int age) : score(score), age(age) {}
 };
 
-//! score for adding vertex
+// score for adding vertex
 std::map<int, vertex> add_score;
 
-//! score for removing vertex
+// score for removing vertex
 std::map<int, vertex> remove_score;
 
-//! weight for each vertex
+// weight for each vertex
 std::vector<int> weight;
 
-//! weight mean
+// weight mean
 double mean;
 
-//! update weight
+// update weight
 double gama = 0.5;
 double roi = 0.3;
 
-//! CC strategy
+// CC strategy
 std::vector<bool> cc;
-//! tabu strategy 未来 n 次内不能被选中删除
+// tabu strategy 未来 n 次内不能被选中删除
 std::vector<short> tabu;
 
-//! best solution
+// best solution
 std::unordered_set<int, custom_hash> C_star;
 
-//! random seed
+// random seed
 std::random_device rd;
 std::knuth_b gen(rd());
 
-//! standard answer
+// standard answer
 int standard_answer;
 
-#endif //PDSP_BASIC_H
+#endif  // PDSP_BASIC_H
--- main.cpp
+++ main.cpp
@@ -1,67 +1,107 @@
+//! @file main program
+//! @author Virgil
+#include <chrono>
+#include <fstream>
+#include "basic.h"
 #include "checker.h"
 #include "pdsp.h"
-#include <chrono>
 
-//! TODO: refactor this project
+// TODO refactor this project
 
-//! build the initial graph
-//! TODO: change when change test case
+// TODO add comments for each function
+
+// TAG change when change test case
+//! @fn init
+//! @brief initialize the graph with console input
 void init() {
-    std::cin >> standard_answer;
-    std::cin >> n >> m;
-    weight.resize(n + 1);
-    cc.resize(n + 1);
-    tabu.resize(n + 1);
-    std::fill(weight.begin(), weight.end(), true);
-    std::fill(cc.begin(), cc.end(), 1);
-    for (int i = 0; i < m; i++) {
-        int from, to;
-        std::cin >> from >> to;
-        edges[from].emplace_back(to);
-        edges[to].emplace_back(from);
-        neighbor[from].emplace_back(to);
-        neighbor[to].emplace_back(from);
-    }
-    //! for IEEE test case
-    //! for other test case
-/*    for (int v = 0; v < n; v++) {*/
-    for (int v = 1; v <= n; v++) {
-        Link[v] = 0;
-        int score = 0;
-        for (auto &u: neighbor[v])
-            score += weight[u];
-        score += weight[v];
-        add_score.insert({v, vertex{score, 0}});
-    }
-    mean = 1;
+  std::cin >> standard_answer;
+  std::cin >> n >> m;
+  weight.resize(n + 1);
+  cc.resize(n + 1);
+  tabu.resize(n + 1);
+  std::fill(weight.begin(), weight.end(), true);
+  std::fill(cc.begin(), cc.end(), 1);
+  for (int i = 0; i < m; i++) {
+    int from, to;
+    std::cin >> from >> to;
+    edges[from].emplace_back(to);
+    edges[to].emplace_back(from);
+    neighbor[from].emplace_back(to);
+    neighbor[to].emplace_back(from);
+  }
+  //! for IEEE test case
+  //! for other test case
+  /*    for (int v = 0; v < n; v++) {*/
+  for (int v = 1; v <= n; v++) {
+    Link[v] = 0;
+    int score = 0;
+    for (auto &u : neighbor[v]) score += weight[u];
+    score += weight[v];
+    add_score.insert({v, vertex{score, 0}});
+  }
+  mean = 1;
+}
+
+// DONE (virgil) Add an initialization function for reading from a file
+//! @fn init
+//! @brief initialize the graph with file input
+void init(std::ifstream &input) {
+  input >> standard_answer;
+  input >> n >> m;
+  weight.resize(n + 1);
+  cc.resize(n + 1);
+  tabu.resize(n + 1);
+  std::fill(weight.begin(), weight.end(), true);
+  std::fill(cc.begin(), cc.end(), 1);
+  for (int i = 0; i < m; i++) {
+    int from, to;
+    input >> from >> to;
+    edges[from].emplace_back(to);
+    edges[to].emplace_back(from);
+    neighbor[from].emplace_back(to);
+    neighbor[to].emplace_back(from);
+  }
+  //! for IEEE test case
+  //! for other test case
+  /*    for (int v = 0; v < n; v++) {*/
+  for (int v = 1; v <= n; v++) {
+    Link[v] = 0;
+    int score = 0;
+    for (auto &u : neighbor[v]) score += weight[u];
+    score += weight[v];
+    add_score.insert({v, vertex{score, 0}});
+  }
+  mean = 1;
 }
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    /*std::ifstream ifs("example.in");*/
-    init();
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+#if defined(__DEBUG__)
+  std::ifstream ifs("../testcase/IEEE-30.graph");
+  init(ifs);
+#endif
+  init();
 
-    auto start = std::chrono::steady_clock::now();
-    pdsp();
-    auto end = std::chrono::steady_clock::now();
-    std::cout << "Elapsed time in milliseconds: "
-              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
-              << " us" << std::endl;
-    // debug: test case is IEEE-30.graph
-    /*add(6);
-    add(10);
-    add(27);
-    std::cout << Link[12];
-    remove(6);
-    std::cout << '\n' << Link[12] << '\n';
-    for (auto &i: P) {
-        std::cout << i << " ";
-    }*/
-    checker c(C);
-    c.check();
-    c.is_correct();
-    return 0;
+  auto start = std::chrono::steady_clock::now();
+  pdsp();
+  auto end = std::chrono::steady_clock::now();
+  std::cout << "Elapsed time in milliseconds: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " us" << std::endl;
+  // debug: test case is IEEE-30.graph
+  /*add(6);
+  add(10);
+  add(27);
+  std::cout << Link[12];
+  remove(6);
+  std::cout << '\n' << Link[12] << '\n';
+  for (auto &i: P) {
+      std::cout << i << " ";
+  }*/
+  checker c(C);
+  c.check();
+  c.is_correct();
+  return 0;
 }
 
 /*
