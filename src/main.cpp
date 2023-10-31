b32a0d2bdf6b3c86d6713e893161c27045bc3f72
Virgil <virgiling7@gmail.com>
2023-10-31 20:00:57 +0800

    feat: deal intermediate result

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,24 @@
 #include <iostream>
 
 int main(int argc, const char *argv[]) {
-  if (argc <= 1) {
+
+  if (argc <= 2) {
     exit(1);
   }
+
   std::ifstream fin(argv[1]);
+  std::ofstream fout(argv[2]);
+
   NuPDS pds;
   pds.init(fin);
   pds.solve();
 
   auto solution = pds.get_best_solution();
-  std::cout << solution.size() << std::endl;
+  fout << solution.size() << std::endl;
   for (auto &v : solution) {
-    std::cout << v << std::endl;
+    fout << v << " ";
   }
+  fout << std::endl;
 
   return 0;
 }
